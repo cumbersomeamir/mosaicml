f4f75479ce425df43bc68ee813bc9aa5fc567395
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-05-18 23:21:49 +0100

    Update mosaicml-7b-ailogy.py

--- mosaicml-7b-ailogy.py
+++ mosaicml-7b-ailogy.py
@@ -1,4 +1,4 @@
-!pip3 install torch transformers einops
+#!pip3 install torch transformers einops
 
 import torch
 import transformers
@@ -7,4 +7,24 @@ model = transformers.AutoModelForCausalLM.from_pretrained("mosaicml/mpt-7b",trus
 
 model.eval()
 model.to("cuda:0")
+
+model_size = sum(t.numel() for t in model.parameters())
+print(f"Model Size: {model_size/1000**2:.1f} M parameters")
+
+tokenizer = transformers.AutoTokenizer.from_pretrained("mosaicml/mpt-7b")
+
+txt = "This is a text to text mosaic"
+
+tokenized_example = tokenizer(txt, return_tensors="pt")
+
+
+print(tokenized_example['input_ids']
+      
+model.generate(tokenized_example['input_ids'].to('cuda:0'), max_new_tokens =150, do_sample = False, top_k = 5, top_p = 0.95)
+
+answer = tokenizer.batch_decode(outputs, skip_special_tokens = True)
+      
+print(answer[0].rstrip())
+      
+
 
